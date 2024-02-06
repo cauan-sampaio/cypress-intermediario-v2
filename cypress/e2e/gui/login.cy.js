ca6127876e342f6bb8a81daf35b301e907a20325
Cauan <76559406+cauan-sampaio@users.noreply.github.com>
2024-02-06 10:44:23 -0300

    oi

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,8 +1,10 @@
 describe('Login teste', () => {
   const data = require('../../../cypress.env.json')
   it('passes', () => {
-    cy.visit('/')
-    cy.login()
+    const user = Cypress.env('user_name')
+    const password = Cypress.env('user_password')
+    const options = { cacheSession: true }
+    cy.login(user, password, options)
     cy.get('.blank-state-welcome-title').contains("Welcome to GitLab")
     cy.url().should('eq', 'http://localhost/users/sign_in');
 
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,26 +1,39 @@
-
-
 Cypress.Commands.add('login', (
-    user = Cypress.env('user_name'),
-    password = Cypress.env('user_password'),
-  ) => {
-    const login = () => {
-      cy.visit('/users/sign_in')
-  
-      cy.get("[data-qa-selector='login_field']").type(user)
-      cy.get("[data-qa-selector='password_field']").type(password, { log: false })
-      cy.get("[data-qa-selector='sign_in_button']").click()
-    }
-  
-    login()
-  })
+  user = Cypress.env('user_name'),
+  password = Cypress.env('user_password'),
+  { cacheSession = true } = {},
+) => {
+  const login = () => {
+    cy.visit('/users/sign_in')
 
+    cy.get("[data-qa-selector='login_field']").type(user)
+    cy.get("[data-qa-selector='password_field']").type(password, { log: false })
+    cy.get("[data-qa-selector='sign_in_button']").click()
+  }
+
+  const validate = () => {
+    cy.visit('/')
+    cy.location('pathname', { timeout: 1000 })
+      .should('not.eq', '/users/sign_in')
+  }
+
+  const options = {
+    cacheAcrossSpecs: true,
+    validate,
+  }
+
+  if (cacheSession) {
+    cy.session(user, login, options)
+  } else {
+    login()
+  }
+})
   Cypress.Commands.add('Logout', (
   ) => {
 
    const Logout = () =>{
-    cy.get('.header-user-dropdown-toggle').click()
-    cy.get('.sign-out-link').click()
+    cy.get('.qa-user-avatar').click()
+    cy.contains('Sign out').click()
    }
     Logout()
   })
